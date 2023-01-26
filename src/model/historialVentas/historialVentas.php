2fededf61eecf4417dbc20e8aa3acf8657103455
AGUIZAV15 <95836801+AGUIZAV15@users.noreply.github.com>
2023-01-26 16:36:18 -0600

    cambios en links de accesos

--- src/model/historialVentas/historialVentas.php
+++ src/model/historialVentas/historialVentas.php
@@ -5,15 +5,15 @@ include "../../templates/header.php";
 <!-- MENU PRINCIPAL DE LA PAGINA-->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/POS/index.php">DICAC</a>    
+    <a class="navbar-brand" href="/PointSale/index.php">DICAC</a>    
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapse" aria-controls="collapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button> 
     <div class="collapse navbar-collapse" id="collapse">
       <div class="navbar-nav">
-        <a class="nav-link" aria-current="page" href="/POS/src/model/ventas/vender_productos.php">VENTAS</a>
-        <a class="nav-link" href="/POS/src/model/productos/listar_productos.php">PRODUCTOS</a>
-        <a class="nav-link active" href="/POS/src/model/historialVentas/historialVentas.php">HISTORIAL</a>
+        <a class="nav-link" aria-current="page" href="/PointSale/src/model/ventas/vender_productos.php">VENTAS</a>
+        <a class="nav-link" href="/PointSale/src/model/productos/listar_productos.php">PRODUCTOS</a>
+        <a class="nav-link active" href="/PointSale/src/model/historialVentas/historialVentas.php">HISTORIAL</a>
         <!-- <a class="nav-link" href="#">Precios</a>
         <a class="nav-link disabled">Deshabilitado</a> -->
       </div>
@@ -49,17 +49,17 @@ include "../../templates/header.php";
 
 <script>
   $('document').ready(function(){
-    $('#rellenarVentas').load('/POS/src/controller/listar_ventas_efectuadas.php');
+    $('#rellenarVentas').load('/PointSale/src/controller/listar_ventas_efectuadas.php');
   });
   
   function cancelarVenta(id){
-    $.post("/POS/src/controller/eliminar_venta.php",
+    $.post("/PointSale/src/controller/eliminar_venta.php",
   {
     id
   },
   function(data, status){   
     if(data === 'ok'){      
-      $('#rellenarVentas').load('/POS/src/controller/listar_ventas_efectuadas.php');
+      $('#rellenarVentas').load('/PointSale/src/controller/listar_ventas_efectuadas.php');
     }else{
       console.log("error");
     }
